438d5947713437e840aa5f243bc2038b879189e9
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-03-24 10:52:56 +0430

    git init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    about
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
